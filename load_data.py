9d079c42383ed33e5a5686aeef98db3a9dc827a4
Jordan <jordan.barkai@gmail.com>
2021-03-04 18:32:28 +0100

    smoothin and inserting mock galax into slice

--- load_data.py
+++ load_data.py
@@ -1,35 +1,30 @@
-from astropy.io import fits
-from astropy.utils.data import get_pkg_data_filename
-from spectral_cube import SpectralCube
-from astropy.utils.data import download_file
+
+from os import listdir
+from random import sample
+import numpy as np
 from astropy import units as u
-from astropy import wcs
+from cube import Cube
 
 
-def load_data():
-    # Get Noise
-    noise_data = fits.open('data/U6805.lmap.fits')
-    noise_data[0].header['CTYPE3'] = 'FREQ'
-    noise_data[0].header['CUNIT3'] = 'Hz'
-    rest_freq = noise_data[0].header['FREQR']*u.Hz
-    noise_cube = SpectralCube.read(noise_data)
-    noise_data.close()
-    # Get HI data
-    hi_datafile = download_file(
-    'http://data.astropy.org/tutorials/FITS-cubes/reduced_TAN_C14.fits',
-    cache=True, show_progress=True)
-    hi_data = fits.open(hi_datafile)  # Open the FITS file for reading
-    hi_cube = SpectralCube.read(hi_data)  # Initiate a SpectralCube
-    hi_data.close()  # Close the FITS file - we already read it in and don't need it anymore!
-    return noise_cube, hi_cube, rest_freq
-    
-# If you want to take a subcube
-# _, b, _ = cube.world[0, :, 0]  #extract latitude world coordinates from cube
-# _, _, l = cube.world[0, 0, :]  #extract longitude world coordinates from cube
-# # Define desired latitude and longitude range
-# lat_range = [-46, -40] * u.deg
-# lon_range = [306, 295] * u.deg
-
-# Create a sub_cube cut to these coordinates
-# sub_cube = cube.subcube(xlo=lon_range[0], xhi=lon_range[1], ylo=lat_range[0], yhi=lat_range[1])
-# sub_cube_slab = sub_cube.spectral_slab(-300. *u.km / u.s, 300. *u.km / u.s)
+def create_fake_cube(noise_file, gal_dir, no_gal, freq_slice=471):
+    # Load noise cube
+    noise_cube = Cube(noise_file)
+    noise_cube.load_cube()
+    # Remove existing sources
+    noise_cube.create_mask()
+    noise_cube.cube_data = noise_cube.cube_data - noise_cube.masked
+    noise_slice = noise_cube.cube_data[freq_slice, :, :]
+    # Choose a random sample of mock galaxies
+    filenames = sample(listdir(gal_dir), no_gal)
+    for filename in filenames:
+        # Load galaxy cube
+        gal_cube = Cube(filename)
+        gal_cube.load_cube()
+        # Smooth each galaxy
+        gal_cube.smooth_cube()
+        # Insert in random location
+        im = gal_cube.convolved_image
+        # Create random location to insert
+        insert_loc = noise_slice[100:100+im.shape[0], 100:100+im.shape[1]]
+        noise_slice[100:100+im.shape[0], 100:100+im.shape[1]] = im + insert_loc
+    return noise_slice
--- run.py
+++ run.py
@@ -1,48 +1,8 @@
-#%%
 from load_data import load_data
-import matplotlib.pyplot as plt
-from astropy import units as u
-from astropy.visualization import astropy_mpl_style
 
-
-def plot_cube(title, cube_data, mask=0, vmin=0, vmax=200, rest_value=None, velocity_convention=None):
-    spec_cube = cube_data.with_spectral_unit(u.km/u.s, rest_value=rest_value, velocity_convention=velocity_convention)
-    moment_0 = spec_cube.moment(order=0)  # Zero-th moment
-    moment_1 = spec_cube.moment(order=1)  # First moment
-    hi_column_density = moment_0 * 1.82 * 10**18 / (u.cm * u.cm) * u.s / u.K / u.km
-    # Initiate a figure and axis object with WCS projection information
-    fig = plt.figure(figsize=(10, 5))
-    ax = fig.add_subplot(111, projection=moment_1.wcs)
-
-    # Display the moment map image
-    im = ax.imshow(moment_1.hdu.data, cmap='RdBu_r', vmin=vmin, vmax=vmax)
-    ax.invert_yaxis()  # Flips the Y axis
-
-    # Add axes labels
-    ax.set_xlabel("Galactic Longitude (degrees)", fontsize=16)
-    ax.set_ylabel("Galactic Latitude (degrees)", fontsize=16)
-
-    # Add a colorbar
-    cbar = plt.colorbar(im, pad=.07)
-    cbar.set_label('Velocity (km/s)', size=16)
-
-    # Overlay set of RA/Dec Axes
-    overlay = ax.get_coords_overlay('fk5')
-    overlay.grid(color='white', ls='dotted', lw=2)
-    overlay[0].set_axislabel('Right Ascension (J2000)', fontsize=16)
-    overlay[1].set_axislabel('Declination (J2000)', fontsize=16)
-    # Overplot column density contours
-    if mask == 0:
-        ax.contour(hi_column_density, cmap='Greys_r')
-    else:
-        ax.contour(mask, cmap='Greys_r')
-    fig.tight_layout()
-    fig.savefig('Plots/%s.png'%title)
 
 def main():
-    noise_cube, hi_cube, rest_freq = load_data()
-    plot_cube("noise", noise_cube, vmin=-2, vmax=2, rest_value=rest_freq, velocity_convention=u.doppler_radio)
-    plot_cube("HI", hi_cube)
+    # create_fake_cube(noise_file, gal_dir, no_gal)
     return 0
 
 if __name__ == "__main__":
