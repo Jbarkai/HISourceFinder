smoothing and scaling correct for loading into apertif cubes but very slow for saving fits files

cube.py
@@ -26,16 +26,32 @@ class Cube:
         self.cube_data = hi_data[0].data
         hi_data.close()
 
-    def smooth_cube(self, telescope_resolution=15*u.arcsecond, shrink=False, shrink_xy=[50, 50]):
+    def rescale_cube(self, dim=(512, 512)):
+        # Resize image
+        img_stack_sm = np.zeros((self.cube_data.shape[0], dim[0], dim[1]))
+        for idx in range(len(self.cube_data)):
+            img = self.cube_data[idx, :, :]
+            img_sm = cv2.resize(img, dim, interpolation=cv2.INTER_CUBIC)
+            img_stack_sm[idx, :, :] = img_sm
+        self.cube_data = img_stack_sm
+
+    def smooth_cube(self, telescope_resolution=15*u.arcsecond):
         # calculate the sigma in pixels.
         # Smooth using Gaussian with sigma = tel_res/pixel_size in each direction
-        sigma1 = telescope_resolution.to('deg').value/self.header['CDELT1']
-        sigma2 = telescope_resolution.to('deg').value/self.header['CDELT2']
-        sigma3 = telescope_resolution.to('deg').value/self.header['CDELT3']
+        ts = telescope_resolution.to('deg').value
+        sigma1 = ts/self.header['CDELT1']
+        sigma2 = ts/self.header['CDELT2']
+        sigma3 = ts/self.header['CDELT3']
         convolved_image = ndi.gaussian_filter(self.cube_data, sigma=(sigma1, sigma2, sigma3), order=0)
-        if shrink:
-            convolved_image = cv2.resize(convolved_image, dsize=(50, 50), interpolation=cv2.INTER_CUBIC)
         self.cube_data = convolved_image
+
+    def crop_cube(self, scale=1):
+        # Crop around galaxy
+        true_points = np.argwhere(self.cube_data > np.mean(self.cube_data))
+        c1 = true_points.min(axis=0)
+        c2 = true_points.max(axis=0)
+        cropped = self.cube_data[:, c1[1]:c2[1]+1, c1[2]:c2[2]+1]
+        self.cube_data = cropped
 
     def create_mask(self, scale=1):
         # Find Sources

make_cubes.py
@@ -7,33 +7,36 @@ from cube import Cube
 from random import uniform
 
 
-def create_fake_cube(noise_file, set_wcs=None):
+def create_fake_cube(noise_file, set_wcs=None, reproject=False, scale=1, ctype=False):
     # Load noise cube
-    print("Loading %s..."%noise_file)
     noise_cube = Cube(noise_file, set_wcs)
-    noise_cube.load_cube()
-    empty_cube = noise_cube.cube_data - noise_cube.cube_data
+    noise_cube.load_cube(ctype=ctype)
+    # Take subset for resources for now
+    noise_cube.cube_data = noise_cube.cube_data[:, 800:1312, 800:1312]
+    dim = noise_cube.cube_data.shape[1:]
+    empty_cube = np.zeros(noise_cube.cube_data.shape)
     # # Remove existing sources
     # noise_cube.create_mask()
     # noise_cube.cube_data = noise_cube.cube_data - noise_cube.masked
     # Choose a random sample of mock galaxies
-    print("Adding Galaxies...")
-    files = [filename for filename in listdir('data') if "model" in filename]
-    for filename in files:
-        print("Adding Galaxy %s "%filename, files.index(filename)+1, "out of %s"%len(files))
-        gal_cube = Cube("data/"+filename)
-        gal_cube.load_cube(ctype=True)
-        # Subcube (need to make this dependent on galaxy size)
-        gal_cube.cube_data = gal_cube.cube_data[90:110, 150:250, 150:250]
-        gal_cube.smooth_cube()
-        gal_cube.create_mask(scale=1e2)
-        # Randomly place galaxy in x and y direction and fill whole z
-        mk, mi, mj = gal_cube.masked.shape
-        si = int(uniform(0, noise_cube.cube_data.shape[1]-mi))
-        sj = int(uniform(0, noise_cube.cube_data.shape[2]-mj))
-        sk = int(uniform(0, noise_cube.cube_data.shape[0]-mk))
-        in_loc = noise_cube.cube_data[sk:sk+mk, si:si+mi, sj:sj+mj]
-        empt_loc = empty_cube[sk:sk+mk, si:si+mi, sj:sj+mj]
-        noise_cube.cube_data[sk:sk+mk, si:si+mi, sj:sj+mj] = gal_cube.masked + in_loc
-        empty_cube[sk:sk+mk, si:si+mi, sj:sj+mj] = gal_cube.masked + empt_loc
+    # print("Adding Galaxy %s "%filename, files.index(filename)+1, "out of %s"%len(files))
+    [insert_gal(f, noise_cube.cube_data, empty_cube, dim, reproject, scale) for f in listdir('data/mock_gals')]
     return noise_cube, empty_cube
+
+def insert_gal(filename, cube_data, empty_cube, dim, reproject=True, scale=1):
+    gal_cube = Cube("data/mock_gals/"+filename)
+    gal_cube.load_cube(ctype=True)
+    # gal_cube.cube_data = gal_cube.cube_data[90:110, :, :]
+    if reproject:
+        gal_cube.rescale_cube(dim=dim)
+        gal_cube.smooth_cube()
+    gal_cube.crop_cube(scale=scale)
+    # Randomly place galaxy in x and y direction and fill whole z
+    mk, mi, mj = gal_cube.cube_data.shape
+    si = int(uniform(0, cube_data.shape[1]-mi))
+    sj = int(uniform(0, cube_data.shape[2]-mj))
+    sk = int(uniform(0, cube_data.shape[0]-mk))
+    in_loc = cube_data[sk:sk+mk, si:si+mi, sj:sj+mj]
+    cube_data[sk:sk+mk, si:si+mi, sj:sj+mj] = gal_cube.cube_data + in_loc
+    empt_loc = empty_cube[sk:sk+mk, si:si+mi, sj:sj+mj]
+    empty_cube[sk:sk+mk, si:si+mi, sj:sj+mj] = gal_cube.cube_data + empt_loc