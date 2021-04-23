368bf4b220a1b66b4ff7bb044cea10e9c93b7ba8
J.A. Barkai <barkai@shoemaker.intra.astro.rug.nl>
2021-04-23 18:53:38 +0200

    fix scaling

--- data_generators/scale_cubes.py
+++ data_generators/scale_cubes.py
@@ -4,19 +4,20 @@ import numpy as np
 import argparse
 
 def main(filename, scale):
-    cube_data = fits.getdata("./Input/" + filename)
-    noise_file = filename.split("_")[-1].split(".fits")[0]+".derip.fits"
+    print(filename)
+    cube_data = fits.getdata(filename)
+    noise_file = filename.split("/")[-1].split("_")[-1].split(".fits")[0]+".derip.fits"
     noise_data = fits.getdata("../data/mosaics/" + noise_file)
     if scale == "soft":
         cube_data += noise_data*1e-1
     elif scale == "loud":
         cube_data += noise_data*4e-1
-    fits.writeto(scale+"Input/"+scale+"_"+filename.split("_")[-1], cube_data)
+    fits.writeto("../data/training/" + scale+"Input/"+scale+"_"+filename.split("_")[-1], cube_data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Scale cubes")
     parser.add_argument(
-        '--filename', type=str, nargs='?', const='default', default='noisefree_1245mosB.fits',
+        '--filename', type=str, nargs='?', const='default', default='loud',
         help='Filename')
     parser.add_argument(
         '--scale', type=str, nargs='?', const='default', default='noisefree_1245mosB.fits',
@@ -24,4 +25,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(
-        args.filename)
+        args.filename, args.scale)
