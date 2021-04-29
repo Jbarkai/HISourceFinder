dac4b5e6ec6d582921d421e2498132ce76e923d9
Jordan <jordan.barkai@gmail.com>
2021-04-29 09:06:59 +0200

    fix pixel perc

--- data_generators/explore_data.py
+++ data_generators/explore_data.py
@@ -18,29 +18,29 @@ def get_plot_data(mask, eccentricity, flatness, vol, galdim, tot_flux, peak_flux
     some_props = regionprops(object_labels)
     del object_labels
     gc.collect()
-    # eigen_vals = [gal.inertia_tensor_eigvals for gal in some_props]
+    eigen_vals = [gal.inertia_tensor_eigvals for gal in some_props]
     galdims = [gal.image.shape for gal in some_props]
-    # bbs = [gal.bbox for gal in some_props]
+    bbs = [gal.bbox for gal in some_props]
     del some_props
     gc.collect()
 
-    # tot_fluxes = [np.sum(cube_data[bbs[i][0]:bbs[i][3], bbs[i][1]:bbs[i][4], bbs[i][2]:bbs[i][5]])
-    #  for i in range(len(bbs))]
-    # peak_fluxes = [np.max(np.sum(cube_data[bbs[i][0]:bbs[i][3], bbs[i][1]:bbs[i][4], bbs[i][2]:bbs[i][5]], axis=0))
-    #  for i in range(len(bbs))]
-    # tot_flux.append(tot_fluxes)
-    # peak_flux.append(peak_fluxes)
+    tot_fluxes = [np.sum(cube_data[bbs[i][0]:bbs[i][3], bbs[i][1]:bbs[i][4], bbs[i][2]:bbs[i][5]])
+     for i in range(len(bbs))]
+    peak_fluxes = [np.max(np.sum(cube_data[bbs[i][0]:bbs[i][3], bbs[i][1]:bbs[i][4], bbs[i][2]:bbs[i][5]], axis=0))
+     for i in range(len(bbs))]
+    tot_flux.append(tot_fluxes)
+    peak_flux.append(peak_fluxes)
     del cube_data
     gc.collect()
 
-    # eccentricities = [e[0]/e[1] for e in eigen_vals]
-    # flatnesses = [e[1]/e[2] for e in eigen_vals]
+    eccentricities = [e[0]/e[1] for e in eigen_vals]
+    flatnesses = [e[1]/e[2] for e in eigen_vals]
     vols = [np.prod(gal) for gal in galdims]
     pixel_percent = np.sum(vols)/np.prod(shape_needed)
-    # eccentricity.append(eccentricities)
-    # flatness.append(flatnesses)
-    # vol.append(vols)
-    # galdim.append(galdims)
+    eccentricity.append(eccentricities)
+    flatness.append(flatnesses)
+    vol.append(vols)
+    galdim.append(galdims)
     pixel_percents.append(pixel_percent)
 
 
@@ -69,53 +69,53 @@ def main(root, output_dir):
     plt.show()
     fig3.savefig(output_dir+'pixel_percents.png')
 
-    # fig1 = plt.gcf()
-    # plt.boxplot(tot_flux)
-    # plt.xlabel("Synthetic Noise-free Cube")
-    # plt.ylabel("Total Flux")
-    # plt.show()
-    # fig1.savefig(output_dir+'tot_flux.png')
+    fig1 = plt.gcf()
+    plt.boxplot(tot_flux)
+    plt.xlabel("Synthetic Noise-free Cube")
+    plt.ylabel("Total Flux")
+    plt.show()
+    fig1.savefig(output_dir+'tot_flux.png')
 
-    # fig1 = plt.gcf()
-    # plt.boxplot(peak_flux)
-    # plt.xlabel("Synthetic Noise-free Cube")
-    # plt.ylabel("Peak Flux")
-    # plt.show()
-    # fig1.savefig(output_dir+'peak_flux.png')
+    fig1 = plt.gcf()
+    plt.boxplot(peak_flux)
+    plt.xlabel("Synthetic Noise-free Cube")
+    plt.ylabel("Peak Flux")
+    plt.show()
+    fig1.savefig(output_dir+'peak_flux.png')
 
-    # fig1 = plt.gcf()
-    # plt.boxplot(eccentricity)
-    # plt.xlabel("Synthetic Noise-free Cube")
-    # plt.ylabel("Eccentricity")
-    # plt.show()
-    # fig1.savefig(output_dir+'eccentricity.png')
+    fig1 = plt.gcf()
+    plt.boxplot(eccentricity)
+    plt.xlabel("Synthetic Noise-free Cube")
+    plt.ylabel("Eccentricity")
+    plt.show()
+    fig1.savefig(output_dir+'eccentricity.png')
 
-    # fig1 = plt.gcf()
-    # plt.boxplot(flatness)
-    # plt.xlabel("Synthetic Noise-free Cube")
-    # plt.ylabel("Flatness")
-    # plt.show()
-    # fig1.savefig(output_dir+'flatness.png')
+    fig1 = plt.gcf()
+    plt.boxplot(flatness)
+    plt.xlabel("Synthetic Noise-free Cube")
+    plt.ylabel("Flatness")
+    plt.show()
+    fig1.savefig(output_dir+'flatness.png')
 
-    # fig4 = plt.gcf()
-    # plt.boxplot(vol)
-    # plt.xlabel("Synthetic Noise-free Cube")
-    # plt.ylabel("Volume")
-    # plt.show()
-    # fig4.savefig(output_dir+'vol.png')
+    fig4 = plt.gcf()
+    plt.boxplot(vol)
+    plt.xlabel("Synthetic Noise-free Cube")
+    plt.ylabel("Volume")
+    plt.show()
+    fig4.savefig(output_dir+'vol.png')
 
-    # fig2 = plt.gcf()
-    # fig2, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10,15))
-    # ax1.boxplot([i[0] for i in galdim])
-    # ax2.boxplot([i[1] for i in galdim])
-    # ax3.boxplot([i[2] for i in galdim])
-    # ax1.set_ylabel("Number of Channels")
-    # ax2.set_ylabel("Width")
-    # ax3.set_ylabel("Height")
-    # ax3.set_xlabel("Synthetic Noise-free Cube")
-    # fig2.tight_layout()
-    # plt.show()
-    # fig2.savefig(output_dir+'galdim.png')
+    fig2 = plt.gcf()
+    fig2, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10,15))
+    ax1.boxplot([i[0] for i in galdim])
+    ax2.boxplot([i[1] for i in galdim])
+    ax3.boxplot([i[2] for i in galdim])
+    ax1.set_ylabel("Number of Channels")
+    ax2.set_ylabel("Width")
+    ax3.set_ylabel("Height")
+    ax3.set_xlabel("Synthetic Noise-free Cube")
+    fig2.tight_layout()
+    plt.show()
+    fig2.savefig(output_dir+'galdim.png')
 
 
 
