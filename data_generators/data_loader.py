ec925404e6d5c6e6bb1055c660e2b80db132a2a0
Jordan <jordan.barkai@gmail.com>
2021-05-05 10:18:20 +0200

    get rid of distance bias

--- data_generators/data_loader.py
+++ data_generators/data_loader.py
@@ -4,6 +4,7 @@ from torch.utils.data import Dataset
 from astropy.io import fits
 from astropy.visualization import ZScaleInterval
 from torch.utils.data import DataLoader
+from random import sample
 from sklearn.model_selection import train_test_split
 from skimage.util.shape import view_as_windows
 import pathlib
@@ -27,7 +28,8 @@ class SegmentationDataSet(Dataset):
                  load=False,
                  root='../HISourceFinder/data/training/',
                  mode="train",
-                 scale="loud"
+                 scale="loud",
+                 train_size=0.6
                  ):
         self.list = []
         self.inputs = inputs
@@ -39,15 +41,14 @@ class SegmentationDataSet(Dataset):
         self.mode = mode
         self.root = root
         self.scale = scale
+        self.train_size = train_size
         self.save_name = self.root + 'hisource-list-' + self.mode + '-slidingwindow.txt'
         if load:
             ## load pre-generated data
-            with open(self.save_name, "rb") as fp:
-                list_file = pickle.load(fp)
-            self.list = list_file
+            self.list = [(inputs[i], targets[i]) for i in range(len(inputs))]
             return
 
-        subvol = '_vol_' + str(dims[0]) + 'x' + str(dims[1]) + 'x' + str(dims[2]) + "_" +self.mode
+        subvol = '_vol_' + str(dims[0]) + 'x' + str(dims[1]) + 'x' + str(dims[2]) + "_" + str(int(self.train_size*100))
         self.sub_vol_path = self.root + '/generated/' + self.scale +"/" + subvol + '/'
         if os.path.exists(self.sub_vol_path):
             shutil.rmtree(self.sub_vol_path)
@@ -61,7 +62,7 @@ class SegmentationDataSet(Dataset):
             filename = self.sub_vol_path + 'cube_' + str(index) +"_subcube_"
             list_saved_cubes = save_sliding_window(input_ID, dims, overlaps, filename, seg=False)
             print("saved %s cubes"%len(list_saved_cubes))
-            list_saved_masks = save_sliding_window(target_ID, dims, overlaps, filename, seg=False)
+            list_saved_masks = save_sliding_window(target_ID, dims, overlaps, filename, seg=True)
             print("saved %s masks"%len(list_saved_masks))
             self.list += [(x, y) for x, y in zip(list_saved_cubes, list_saved_masks)]
         # Save list of subcubes
@@ -76,9 +77,6 @@ class SegmentationDataSet(Dataset):
         # Select the sample
         input_path, seg_path = self.list[index]
         x, y = np.load(input_path), np.load(seg_path)
-        # # Typecasting
-        # input_x = torch.from_numpy(x.astype(np.float32)).type(self.inputs_dtype)
-        # target_y = torch.from_numpy(y.astype(np.int64)).type(self.targets_dtype)
         return torch.FloatTensor(x).unsqueeze(0), torch.FloatTensor(y).unsqueeze(0)
 
 
@@ -106,104 +104,3 @@ def save_sliding_window(input_ID, dims, overlaps, filename, seg=False):
                 count += 1
                 print("\r", count*100/(x*y*z), end="")
     return filelist
-
-
-def main(batch_size, shuffle, num_workers, dims, overlaps, root, random_seed, train_size, scale):
-    """Create training and validation datasets
-
-    Args:
-        batch_size (int): Batch size
-        shuffle (bool): Whether or not to shuffle the train/val split
-        num_workers (int): The number of workers to use
-        dims (list): The dimensions of the subcubes
-        overlaps (list): The dimensions of the overlap of subcubes
-        root (str): The root directory of the data
-        random_seed (int): Random Seed
-        train_size (float): Ratio of training to validation split
-        scale (str): Loud or soft - S-N ratio
-
-    Returns:
-        The training and validation data loaders
-    """
-    # input and target files
-    inputs = [root+scale+'Input/' + x for x in listdir(root+scale+'Input') if ".fits" in x]
-    inputs = sample(inputs, subsample)
-    targets = [root+'Target/mask_' + x.split("/")[-1].split("_")[-1] for x in inputs]
-    print(inputs, targets)
-    inputs_train, inputs_valid = train_test_split(
-        inputs,
-        random_state=random_seed,
-        train_size=train_size,
-        shuffle=True)
-
-    targets_train, targets_valid = train_test_split(
-        targets,
-        random_state=random_seed,
-        train_size=train_size,
-        shuffle=True)
-    # dataset training
-    dataset_train = SegmentationDataSet(inputs=inputs_train,
-                                        targets=targets_train,
-                                        dims=dims,
-                                        overlaps=overlaps,
-                                        load=False,
-                                        root=root,
-                                        mode="train",
-                                        scale="loud")
-
-    # dataset validation
-    dataset_valid = SegmentationDataSet(inputs=inputs_valid,
-                                        targets=targets_valid,
-                                        dims=dims,
-                                        overlaps=overlaps,
-                                        load=False,
-                                        root=root,
-                                        mode="test",
-                                        scale="loud")
-
-    # dataloader training
-    params = {'batch_size': batch_size,
-            'shuffle': shuffle,
-            'num_workers': num_workers}
-    dataloader_training = DataLoader(dataset=dataset_train, **params)
-
-    # dataloader validation
-    dataloader_validation = DataLoader(dataset=dataset_valid, **params)
-    return dataloader_training, dataloader_validation
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create training and validation datasets",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(
-        '--batch_size', type=int, nargs='?', const='default', default=1,
-        help='Batch size')
-    parser.add_argument(
-        '--shuffle', type=bool, nargs='?', const='default', default=True,
-        help='Whether or not to shuffle the train/val split')
-    parser.add_argument(
-        '--num_workers', type=int, nargs='?', const='default', default=2,
-        help='The number of workers to use')
-    parser.add_argument(
-        '--dims', type=list, nargs='?', const='default', default=[128, 128, 64],
-        help='The dimensions of the subcubes')
-    parser.add_argument(
-        '--overlaps', type=list, nargs='?', const='default', default=[15, 20, 20],
-        help='The dimensions of the overlap of subcubes')
-    parser.add_argument(
-        '--root', type=str, nargs='?', const='default', default='./data/training/',
-        help='The root directory of the data')
-    parser.add_argument(
-        '--random_seed', type=int, nargs='?', const='default', default=42,
-        help='Random Seed')
-    parser.add_argument(
-        '--scale', type=str, nargs='?', const='default', default="loud",
-        help='The scale of inserted galaxies to noise')
-    parser.add_argument(
-        '--train_size', type=float, nargs='?', const='default', default=0.6,
-        help='Ratio of training to validation split')
-    args = parser.parse_args()
-
-    main(
-        args.batch_size, args.shuffle, args.num_workers, args.dims,
-        args.overlaps, args.root, args.random_seed, args.train_size, args.scale)
