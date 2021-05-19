7bbd21b2d327ba1848134b2773e6d3fc0d7ac696
Jordan <jordan.barkai@gmail.com>
2021-05-19 10:58:11 +0200

    add corners for already existing cubes

--- data_generators/scale_cubes.py
+++ data_generators/scale_cubes.py
@@ -9,29 +9,29 @@ def main(filename, scale):
     print(filename)
     cube_data = fits.getdata(filename)
     noise_file = filename.split("/")[-1].split("_")[-1].split(".fits")[0]+".derip.fits"
+    print(noise_file)
     hdul = fits.open("./data/mosaics/" + noise_file)
     hdr = hdul[0].header
-    orig_d = 50*u.Mpc
+    hdul.close()
     dx = np.abs(hdr["CDELT1"]*u.deg)
-    fwhm_x = (dx).to(u.arcsec)
-    sigma_x = (fwhm_x/np.sqrt(8*np.log(2))).to(u.deg).value
-    noise_data = hdul[0].data
-    if scale == "soft":
-        cube_data += noise_data*1e-1
-    elif scale == "loud":
-        cube_data += noise_data*4e-1
-    noise_corner = np.random.normal(scale=sigma_x/np.sqrt(3.0), size=(100, 800, 1400))
+    sigma_x = (dx/np.sqrt(8*np.log(2))).to(u.deg).value
+    # noise_data = hdul[0].data
+    # if scale == "soft":
+    #     cube_data += noise_data*1e-1
+    # elif scale == "loud":
+    #     cube_data += noise_data*4e-1
+    noise_corner = np.random.normal(scale=sigma_x, size=cube_data.shape)
     cube_data[np.isnan(cube_data)] = noise_corner[np.isnan(cube_data)]
-    fits.writeto("./data/training/"+scale+"Input/"+scale+"_"+filename.split("_")[-1], cube_data, overwrite=True)
+    fits.writeto("./data/training/"+scale+"Input/"+scale+"_"+filename.split("_")[-1], cube_data, header=hdr, overwrite=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Scale cubes",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        '--filename', type=str, nargs='?', const='default', default='loud',
+        '--filename', type=str, nargs='?', const='default', default='noisefree_1245mosB.fits',
         help='Filename')
     parser.add_argument(
-        '--scale', type=str, nargs='?', const='default', default='noisefree_1245mosB.fits',
+        '--scale', type=str, nargs='?', const='default', default='loud',
         help='Scaling amount')
     args = parser.parse_args()
 
