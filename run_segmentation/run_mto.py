9eb4b86abf2c2ee3b5e464565e40b9ed1eafa5d2
Jordan <jordan.barkai@gmail.com>
2021-05-25 00:52:51 +0200

    typo

--- run_segmentation/run_mto.py
+++ run_segmentation/run_mto.py
@@ -69,7 +69,7 @@ def main(mto_dir, param_file, input_dir):
             print("\r", index*100/len(cube_list), "%", end="")
             mto_eval(window, mto_dir, param_file, empty_arr, index)
         out_cube_file = "data/mto_output/mtocubeout_" + f_in.split("/")[-1]
-        fits.writeto(out_cube_file, empty_arr)
+        fits.writeto(out_cube_file, empty_arr, overwrite=True)
         os.system("zip %s %s"%(out_cube_file.replace(".fits", ".zip"), out_cube_file))
     return
 
