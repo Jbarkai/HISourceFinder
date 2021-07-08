fix scaling and corners

data_generators/scale_cubes.py
@@ -17,11 +17,18 @@ def main(filename, scale):
     dx = np.abs(hdr["CDELT1"]*u.deg)
     sigma_x = (dx/np.sqrt(8*np.log(2))).to(u.deg).value
     orig_noise = fits.getdata("./data/orig_mosaics/" + noise_file.replace(".norm", ""))
-    scale_fac = np.nanmean(orig_noise[100, 400:-400, 400:-400]/norm_noise[100, 400:-400, 400:-400])
-    cube_data = cube_data + norm_noise*scale_fac*0.05
+    scale_fac = np.nanmean(norm_noise[:, 400:-400, 400:-400]/orig_noise[:, 400:-400, 400:-400], axis=(1,2))
+    # Create Guassian noise in corners
     noise_corner = np.random.normal(scale=sigma_x, size=cube_data.shape)
-    cube_data[np.isnan(cube_data)] = noise_corner[np.isnan(cube_data)]
-    fits.writeto("./data/training/"+scale+"Input/"+scale+"_"+filename.split("_")[-1], cube_data, header=hdr, overwrite=True)
+    # Scale corner noise
+    noise_corner_scaled = np.array([noise_corner[i]*scale_fac[i] for i in range(noise_corner.shape[0])])
+    # Add corners to noise cube
+    norm_noise[np.isnan(norm_noise)] = noise_corner[np.isnan(noise_corner_scaled)]
+    # Scale galaxies
+    cube_data_scaled = np.array([cube_data[i]*scale_fac[i] for i in range(cube_data.shape[0])])
+    # Add galaxies to cube
+    norm_noise += cube_data_scaled*1e1
+    fits.writeto("./data/training/"+scale+"Input/"+scale+"_"+filename.split("_")[-1], norm_noise, header=hdr, overwrite=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Scale cubes",