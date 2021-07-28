0d4082c1b25d5f9917fc2d2bf2d02535379cb633
Jordan <jordan.barkai@gmail.com>
2021-07-28 14:36:00 +0200

    env setup for peregrine

--- train_model.sh
+++ train_model.sh
@@ -7,11 +7,14 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=9GB
  
-module load Python/3.8.6-GCCcore-10.2.0
-module load cuDNN/5.0-CUDA-7.5.18
+module load Python/3.7.4-GCCcore-8.3.0
+module load TensorFlow/2.3.1-fosscuda-2019b-Python-3.7.4
+module load PyTorch/1.6.0-fosscuda-2019b-Python-3.7.4
+module load scikit-image/0.16.2-fosscuda-2019b-Python-3.7.4
+module load scikit-learn/0.22.2.post1-fosscuda-2019b-Python-3.7.4
  
 source /data/$USER/.envs/vnet/bin/activate
- 
-python3 python train_model.py --nEpochs=2 --terminal_show_freq=25 --scale=loud --k_folds=1 --subsample=1
+
+python3 train_model.py --nEpochs=2 --terminal_show_freq=25 --scale=loud --k_folds=1 --subsample=1
 
 deactivate
